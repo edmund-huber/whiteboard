f7553710776ed902557510e00af994748ad2ba41
Edmund Huber <me@ehuber.info>
2017-03-05 03:43:18 -0800

    never meant to commit this

--- whiteboard.js
+++ whiteboard.js
@@ -6,10 +6,6 @@
     var clear = function() {
         ctx.fillStyle = 'rgb(0, 255, 0)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = 'rgb(255, 0, 0)';
-    ctx.fillRect(canvas.width / 2 - 100, canvas.height / 2 - 100, canvas.width / 2 + 100, canvas.height / 2 + 100);
-
     };
 
     // When the page is first loaded, and every time the window is resized,
