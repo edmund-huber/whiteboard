cdca38cfadbae6aacb34c120d564af1353cfaadf
Edmund Huber <me@ehuber.info>
2017-03-05 03:26:16 -0800

    background green for chroma key

--- whiteboard.js
+++ whiteboard.js
@@ -1,3 +1,6 @@
 (function() {
-    console.log(document.getElementById('canvas'));
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(0, 255, 0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 })();
